c6adf00686785d64998296da333853ad0b117676
JosephHills <josephhills14@hotmail.com>
2021-08-24 15:10:24 +0100

    completed exercise3

---
+++ src/main/java/com/qa/exercise3/CheckString2.java
@@ -0,0 +1,13 @@
+package com.qa.exercise3;
+
+public class CheckString2 {
+
+	public boolean match2(String input1, String input2) {
+		if (input1.equals(input2))
+			return true;
+		else
+			return false;
+
+	}
+
+}
